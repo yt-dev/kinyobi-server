7a692bcf4d9eaaf1a73145cac72c06f4b234d8e8
YT Huang <ytzhbit@icloud.com>
2020-08-21 14:30:10 +0800

    MikroORM Setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroCofig from "./mikro-orm.config";
+
+(async () => {
+  const orm = await MikroORM.init(mikroCofig);
+  await orm.getMigrator().up();
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
+})().catch((err) => console.error(err));
