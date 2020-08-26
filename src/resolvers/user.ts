3cebe56ec97dc3850edda79a3814f35ec6c487e8
YT Huang <ytzhbit@icloud.com>
2020-08-26 20:55:57 +0800

    fix register function

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
 import { MyContext } from "src/types";
 import { User } from "../entities/User";
 import argon2 from "argon2";
+import { EntityManager } from "@mikro-orm/postgresql";
 
 @InputType()
 class UsernamePasswordInput {
@@ -76,15 +77,22 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning("*");
+      user = result[0];
     } catch (err) {
       // duplicate username error
-      if (err.code === "23505" || err.detail.includes("already exists")) {
+      if (err.code === "23505") {
         return {
           errors: [
             {
@@ -94,8 +102,6 @@ export class UserResolver {
           ],
         };
       }
-
-      console.log("message: ", err.message);
     }
 
     req.session.userId = user.id;
